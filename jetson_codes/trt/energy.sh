d2a85729f5a0989d53073a5fb60fa5d305b45ea1
rubende <rubende@uma.es>
2022-02-22 12:49:01 +0100

    update 2022

---
+++ jetson_codes/trt/energy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Energy
+
+# Whole board
+watch -n 0,1 cat /sys/bus/i2c/drivers/ina3221x/1-0040/iio:device0/in_current0_input
+
+# GPU
+#watch -n 0,1 cat /sys/bus/i2c/drivers/ina3221x/1-0040/iio:device0/in_current1_input
+
+# CPU
+#watch -n 0,1 cat /sys/bus/i2c/drivers/ina3221x/1-0040/iio:device0/in_current2_input
